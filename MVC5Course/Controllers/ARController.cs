a780bc9ed2ebc60d8fc39aeeacda5178d8b7c42a
Chris99252 <chris@ccf.org.tw>
2015-04-25 17:31:33 +0800

    Commit 練習 ViewResult 與 ContentResult

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View("View1");
+        }
+
+        public ActionResult View1()
+        {
+            return View();
+        }
+
+        public ActionResult View2()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Content1()
+        {
+            return Content("<script>alert('OK'); location.href='/';</script>","text/html");
+        }
+
+        public ActionResult Content2()
+        {
+            return View("Content2");
+        }
+    }
+}
