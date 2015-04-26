f76b865b85a33bc720d2a3aa08615543c2287072
Chris99252 <chris@ccf.org.tw>
2015-04-26 17:15:36 +0800

    Commit 練習 [HandleError] 自訂錯誤頁面的做法

---
+++ MVC5Course/Controllers/AFController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AFController : Controller
+    {
+        // GET: AF
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Wrong()
+        {
+            throw new Exception("OK");
+            return View();
+        }
+    }
+}
--- MVC5Course/Controllers/BaseController.cs
+++ MVC5Course/Controllers/BaseController.cs
@@ -7,6 +7,10 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
+
+#if !DEBUG
+    [Authorize]
+#endif
     public abstract class BaseController : Controller
     {
         protected FabricsEntities db = new FabricsEntities();
